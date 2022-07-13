e2d3b7f85b42cc1aa244a2ca307eea364116d3b6
LEONARDO OLIVEIRA SOUZA <88562663+Leonardu76@users.noreply.github.com>
2022-07-13 17:58:50 -0300

    login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,27 +1,107 @@
-import React from 'react'
 import './login.css'
+import React, { useState } from "react";
+import {useEffect} from 'react'
+
 import Navbar from '../../components/navbar/navbar'
 
 import { FaFacebook, FaLinkedin, FaGoogle } from 'react-icons/fa';
 
 const login = () => {
   const url = 'login'
+  const [data, setData] = useState([])
+  
+  const [email, setEmail] = useState()
+  const [senha, setSenha] = useState()
+  // const [status, setStatus] = useState({
+  //   type: '',
+  //   mensagem: ''
+  // })
+
+  const GetUser  = async e =>{
+    e.preventDefault()
+
+   let reqs =  await fetch("http://localhost/api/login.php", {
+      method: 'POST',
+      headers: {
+    'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email, senha})
+    })
+    const ret = await reqs.json();
+    console.log(ret)
+
+    // .then((response) =>response.json())
+    // .then((responseJson) => {
+    //   setData(responseJson.user)
+    //       })
+    //       console.log(email)
+
+
+// if(responseJson.erro){
+//   setStatus({
+//     type:'erro', 
+//     mensagem: responseJson.messagem
+//    })
+
+// }else{
+//   setStatus({
+//     type:'sucess', 
+//     mensagem: responseJson.messagem
+//    })
+
+// }
+    
+//     }).catch(() => {
+//       setStatus({
+//        type:'erro', 
+//        mensagem: 'Usuário não encontrado, tente mais tarde!'
+//       })
+//     })
+  }
+          
+        
+
+  const onChangeHandlerEmail = (e) => {
+
+    setEmail(e.target.value)
+
+  }
+
+  const onChangeHandlerSenha = (e) => {
+
+    setSenha(e.target.value)
+
+  }
+
+  // const onButtonClickHandler = () =>{
+  //   useEffect(() => {
+  //     GetUser();
+  //       },[])
+  // }
+
+
+
+
   return (
     <div className='loginBackground'>
           <Navbar url={url}/>
+          {/* <div className={status.type === 'erro' ? "errResponse" : status.type === 'sucess' ? "sucResponse" : null}>
+{status.type === 'erro' ? <p> {status.mensagem}</p> : status.type === 'sucess' ? <p> {alert("Eu sou um alert!")}</p>: null}
+</div> */}
+
 
     <div className='Login-div'>
 
-     <form className='form-login'>  
+     <form className='form-login' onSubmit={GetUser}>  
      <h1 className='Logintitle'>Login</h1>
- 
+ {data}
     <label><b>Email</b></label> 
-    <input className='inputForm' type="email"  />
+    <input className='inputForm' name='email' type="email" onChange={onChangeHandlerEmail} />
 
     <label><b>Senha</b></label> 
-    <input className='inputForm' type="password"  />
+    <input className='inputForm' type="password" name='password'  onChange={onChangeHandlerSenha}/>
 
-    <button className='btnLogin' type='submit'>Login</button>
+    <button className='btnLogin'  >Login</button>
     <p className='text'>Login com redes sociais</p>
 
   <div className='redesLogin'>
